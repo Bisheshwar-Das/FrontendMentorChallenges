6622687bd86db3fd6f77a535083f7bb86b054b63
Bisheshwar Das <officialbisheshwar@gmail.com>
2024-06-23 13:39:13 +0545

    done with html and css, done with selecting rating for 'Interactive rating component

--- interactive-rating-component/interactive-rating-componenet.js
+++ interactive-rating-component/interactive-rating-componenet.js
@@ -1,1 +1,24 @@
-console.log("COnnected")
+var html=""
+output=document.getElementById("rating-bar");
+for(var i=1;i<=5;i++){
+    html+=`<p class="ratings">${i}</p>`
+}
+output.innerHTML=html;
+
+
+var selected_rating='';
+var rating = document.querySelectorAll(".ratings");
+
+rating.forEach(ratings=>{
+    ratings.addEventListener("click",()=>{
+        if(selected_rating!=ratings.innerHTML){
+            rating.forEach(ratin=>{
+                if(ratin.innerHTML==selected_rating){
+                    ratin.classList.toggle('selected')
+                }
+            })
+            selected_rating=ratings.innerHTML;
+            ratings.classList.toggle('selected')
+        }
+    })
+})
