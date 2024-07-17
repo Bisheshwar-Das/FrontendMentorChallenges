eba18d7e049086f7c342f4e3e368ab0781de245e
Bisheshwar Das <officialbisheshwar@gmail.com>
2024-07-17 18:49:34 +0545

    done wiht 'base apparel coming soon page

--- base-apparel-coming-soon/base-apparel-coming-soon-page.js
+++ base-apparel-coming-soon/base-apparel-coming-soon-page.js
@@ -1,1 +1,16 @@
 console.log("connected")
+function submitit(){
+    const emailInput=document.getElementById('emailinpt').value;
+    const feedbacke=document.getElementById('emailfeedback');
+    const emailRegex = /\S+@\S+\.\S+/;
+    
+    if(emailRegex.test(emailInput)){
+        document.getElementById("err").style.visibility="hidden"
+        console.log("connected")   
+        feedbacke.textContent="";
+}else{
+    console.log(emailInput)   
+    document.getElementById("err").style.visibility="visible"
+    feedbacke.textContent="Please provide a valid email";
+}
+}
